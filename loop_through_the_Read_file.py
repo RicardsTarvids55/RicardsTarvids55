1482ff05f3ec069e9bada6c477a1f77a08e2ee9f
Henrijs2004 <63504576+Henrijs2004@users.noreply.github.com>
2023-11-01 09:21:40 +0200

    appended text

--- loop_through_the_Read_file.py
+++ loop_through_the_Read_file.py
@@ -1,4 +1,7 @@
-f = open("Mans_teksts.txt", "r")
-for x in f:
-    print(x)
+f = open("Mans_teksts.txt", "a")
+f.write("Now the file has more content!")
 f.close()
+
+#open and read the file after the appending:
+f = open("Mans_teksts.txt", "r")
+print(f.read())
