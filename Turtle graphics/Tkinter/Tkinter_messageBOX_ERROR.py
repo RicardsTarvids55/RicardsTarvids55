0af47e46d4c8fcbbd299685bc97d7e42ba8edb5e
Henrijs2004 <63504576+Henrijs2004@users.noreply.github.com>
2024-02-14 08:04:15 +0200

    tkinter errors and warnings

---
+++ Turtle graphics/Tkinter/Tkinter_messageBOX_ERROR.py
@@ -0,0 +1,10 @@
+from tkinter import *  
+  
+from tkinter import messagebox  
+  
+win = Tk()  
+win.geometry("200x200")
+
+messagebox.showerror("Error","There is an ERROR")  
+  
+win.mainloop()
---
+++ Turtle graphics/Tkinter/tkinter_messageBOX_WARNING.py
@@ -0,0 +1,9 @@
+from tkinter import *  
+  
+from tkinter import messagebox  
+  
+win = Tk()  
+win.geometry("200x200")  
+messagebox.showwarning("warning","This is a Warning")  
+  
+win.mainloop()  
